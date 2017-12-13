11922c8c187e6bb607408ab9f55a9042a1a350ac
张娜-class2 <1508956045@qq.com>
2017-12-13 18:40:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
+//严格模式下
 
-const Mime = require('./Mime');
+const Mime = require('./Mime');//引入同级目录下的Mime.js文件
 module.exports = new Mime(require('./types/standard'), require('./types/other'));
+//实例化引入同级目录types文件夹下的standard.json和other.json文件的Mime对象并暴露出来
