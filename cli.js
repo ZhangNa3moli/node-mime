a305b55ee43489d2018ba52ea5798d6d4d54299b
张娜-class2 <1508956045@qq.com>
2017-12-13 18:37:17 +0800

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-
+//Node安装可能会在 /usr/local/bin/ 而不是 /usr/bin/ 就需要 env 命令在前边
 'use strict';
+//严格模式下
 
-var mime = require('.');
-var file = process.argv[2];
-var type = mime.getType(file);
+var mime = require('.');//引入同级目录下的所有文件模块
+var file = process.argv[2];//通过命令行参数获得文件的文件名/路径
+var type = mime.getType(file);//获取给定路径或扩展的MIME类型
 
-process.stdout.write(type + '\n');
+process.stdout.write(type + '\n');//把Mime类型+换行打印出来显示在屏幕上
